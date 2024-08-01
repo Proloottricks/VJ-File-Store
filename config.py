535e00fdd0de206256663afebedba69e44344431
Proloottricks <ritiksakhala17@gmail.com>
2024-08-01 20:50:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,6 +78,6 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
+URL = environ.get("URL", "grateful-ranee-sankarilinks-c995afe8.koyeb.app/")
 
     
